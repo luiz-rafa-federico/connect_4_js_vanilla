449d4e5f600958dfc508f1824abc0ce7a72ea0de
José Flávio <jotaftm@gmail.com>
2021-06-09 11:51:59 -0300

    Finalizado a função que move as peças

--- script.js
+++ script.js
@@ -1,15 +1,49 @@
 // Variáveis globais
 const mapa = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0]
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0]
 ];
+const msgRetorno = document.getElementById("msgRetorno");
 // Variáveis globais
 
+//handler para botao jogar
+const btn_jogar = document.getElementById("btn_jogar");
+btn_jogar.addEventListener("click", function() {
+    const containerJogo = document.getElementById("jogo");
+    containerJogo.classList.remove("displayNone");
+    setTimeout(function() {
+        gerarTabela();
+        gerarPecas();
+    }, 1000);
+});
+//handler para botao jogar
+
+// Construindo tabela
+const gerarTabela = () => {
+    for (let i = 0; i < mapa.length; i++) {
+        const tabela = document.getElementById('tabela');
+        let divLinha = document.createElement('div');
+        divLinha.addEventListener("click", movePeca);
+        divLinha.classList.add('divLinha');
+        divLinha.id = `${i}`;
+        tabela.appendChild(divLinha);
+        for (let j = 0; j < mapa[i].length; j++) {
+            quadrado = document.createElement('div');
+            quadrado.classList.add('quadrado');
+            divLinha.appendChild(quadrado);
+        }
+    }
+};
+// Construindo tabela
+
 //funçao para gerar as peças de cada jogador
+
+let primeiro = 0;
 const gerarPecas = () => {
     const input1 = document.getElementById("jogador1").value;
     const input2 = document.getElementById("jogador2").value;
@@ -28,7 +62,7 @@ const gerarPecas = () => {
     for (let i = 1; i <= 2; i++) {
         for (let j = 0; j < 21; j++) {
             let peca = document.createElement("div");
-            peca.classList.add(`pecaJogador${i}`, `peca${j}_${i}`, 'pecas');
+            peca.classList.add(`pecaJogador${i}`, 'pecas');
             if (i === 1) {
                 pecasJogador1.appendChild(peca);
             } else {
@@ -36,37 +70,50 @@ const gerarPecas = () => {
             }
         }
     }
+    primeiro = Math.floor(Math.random() * 2 + 1);
+    if (primeiro === 1) {
+        msgRetorno.innerHTML = `${input1} começa jogando!`;
+    } else {
+        msgRetorno.innerHTML = `${input2} começa jogando!`;
+    }
 };
 //funçao para gerar as peças de cada jogador
 
-// Construindo tabela
-const gerarTabela = () => {
-    for (let i = 0; i < mapa.length; i++) {
-        const tabela = document.getElementById('tabela');
-        let divLinha = i;
-        divLinha = document.createElement('div');
-        divLinha.style.display = 'flex';
-        divLinha.classList.add('divLinha');
-        tabela.appendChild(divLinha);
-
-        for (let j = 0; j < mapa[i].length; j++) {
-            let quadrado = mapa[i][j];
-            quadrado = document.createElement('div');
-            quadrado.classList.add('quadrado');
-            divLinha.appendChild(quadrado);
+// função pra mover peças
+const movePeca = (event) => {
+    let colunaClicada = event.currentTarget;
+    let filhos = colunaClicada.children;
+    for (let i = 0; i < filhos.length; i++) {
+        if (filhos[i].childElementCount === 0) {
+            let celula = filhos[i];
+            let peca = pecasJogador1.lastElementChild;
+            let indiceColuna = colunaClicada.id;
+            let indiceLinha = i;
+            let numero = 0;
+            if (primeiro % 2 === 0) {
+                peca = pecasJogador2.lastElementChild;
+                celula.appendChild(peca);
+                numero = 2;
+                i = filhos.length;
+            } else {
+                celula.appendChild(peca);
+                numero = 1;
+                i = filhos.length;
+            }
+            mapa[indiceColuna][indiceLinha] = numero;
+            primeiro++;
+            verificaVitoria();
         }
     }
 };
-// Construindo tabela
+//função pra mover peças
 
-//handler para botao jogar
-const btn_jogar = document.getElementById("btn_jogar");
-btn_jogar.addEventListener("click", function() {
-    const containerJogo = document.getElementById("jogo");
-    containerJogo.classList.remove("displayNone");
-    setTimeout(function() {
-        gerarTabela();
-        gerarPecas();
-    }, 1000);
-});
-//handler para botao jogar
+// console.log(`Coluna: ${indiceColuna}`, `Linha: ${indiceLinha}`);
+// console.log(mapa)
+
+const verificaVitoria = () => {
+    // André e Priscila, deixei aqui pra chamar só uma função dentro da função
+    // movePeca, aí dentro dessa função aqui vcs podem chamar todas as funçoes q 
+    // verificam vitória
+    console.log('1 click')
+}
